f25a7214467e2ed3b3921622d9a0092665e44107
Christophe MICHEL <christophe.michel-ext@amundi.com>
2018-06-05 17:13:21 +0200

    [day03] part 1 failing test

---
+++ src/main/java/day3/Delivery.java
@@ -0,0 +1,14 @@
+package day3;
+
+class Delivery {
+
+
+    Delivery(String instructions) {
+
+    }
+
+    int houseCount() {
+        return 0;
+    }
+
+}
---
+++ src/test/java/day3/DeliveryTest.java
@@ -0,0 +1,23 @@
+package day3;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class DeliveryTest {
+
+    @DisplayName("Day 3 part 1 acceptance test")
+    @ParameterizedTest(name = "{0} delivers presents to {1} houses")
+    @CsvSource({
+            ">, 2",
+            "^>v<, 4",
+            "^v^v^v^v^v, 2"
+    })
+    void part1_acceptance_test(String instructions, int expectedHouseCount) {
+        Delivery delivery = new Delivery(instructions);
+        assertThat(delivery.houseCount()).isEqualTo(expectedHouseCount);
+    }
+
+}

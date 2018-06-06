c32ee9b2c3422fe3c73e30fbefe3003650dadad4
Christophe MICHEL <christophe.michel-ext@amundi.com>
2018-06-06 11:08:23 +0200

    [day04] part 1 passing implementation (but way too slow)

--- src/main/java/day4/AdventCoinMiner.java
+++ src/main/java/day4/AdventCoinMiner.java
@@ -1,9 +1,35 @@
 package day4;
 
-public class AdventCoinMiner {
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
-    public int compute(String secretKey) {
-        return 0;
+class AdventCoinMiner {
+
+    int compute(String secretKey) {
+        String candidate;
+        int i = 0;
+        do {
+            candidate = secretKey + i;
+            if (hash(candidate)) {
+                return i;
+            }
+            i++;
+        } while (true);
+    }
+
+    private boolean hash(String candidate) {
+        try {
+            MessageDigest md5 = MessageDigest.getInstance("md5");
+            byte[] digest = md5.digest(candidate.getBytes());
+            return toHexString(digest).startsWith("00000");
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("MD5 indisponible");
+        }
+    }
+
+    private String toHexString(byte[] digest) {
+        return String.format("%032x", new BigInteger(1, digest));
     }
 
 }

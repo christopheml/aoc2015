90c7716b940a9b60c5c43c4c5a1c55caf6c0361a
Christophe MICHEL <christophe.michel-ext@amundi.com>
2018-06-06 10:43:24 +0200

    [day04] part 1 failing test

---
+++ src/main/java/day4/AdventCoinMiner.java
@@ -0,0 +1,9 @@
+package day4;
+
+public class AdventCoinMiner {
+
+    public int compute(String secretKey) {
+        return 0;
+    }
+
+}
---
+++ src/test/java/day4/AdventCoinMinerTest.java
@@ -0,0 +1,22 @@
+package day4;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class AdventCoinMinerTest {
+
+    @DisplayName("Day 4 part 1 acceptance test")
+    @ParameterizedTest(name = "Number for {0} is {1}")
+    @CsvSource({
+            "abcdef, 609043",
+            "pqrstuv, 1048970"
+    })
+    void part1_acceptance_test(String secretKey, int expectedNumber) {
+        AdventCoinMiner miner = new AdventCoinMiner();
+        assertThat(miner.compute(secretKey)).isEqualTo(expectedNumber);
+    }
+
+}

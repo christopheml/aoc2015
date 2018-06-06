adbf319a5ca151912b01ca1cef6b8d88dae290df
Christophe MICHEL <christophe.michel-ext@amundi.com>
2018-06-06 11:34:15 +0200

    [day04] part 1 solution

---
+++ src/main/java/day4/Solution.java
@@ -0,0 +1,11 @@
+package day4;
+
+public class Solution {
+
+    public static void main(String... args) {
+        AdventCoinMiner miner = new AdventCoinMiner();
+        int result = miner.compute("bgvyzdsv");
+        System.out.println("Number for bgvyzdsv is " + result);
+    }
+
+}
